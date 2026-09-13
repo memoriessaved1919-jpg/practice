e918355d90946bc2a7b8e6660dc5fc00b3fae31d
Muhammadumar <memoriessaved1919@gmail.com>
2026-09-13 12:46:04 +0900

    feat:learn python boolean

--- primitive.py
+++ primitive.py
@@ -21,3 +21,22 @@ result = course.upper()
 print(f"title upper: {result}")
 result = course.replace("Python FullStack", "Engineering")
 print(f"title replace: {result}")
+
+
+print("====== boolean ======")
+# function > print(), type(), input(), bool(), int(), str()
+y = input("Give your value for y:")
+print(f"y: {y}")
+
+result = y.isnumeric()
+print(f"The input value is numeric: {result}")
+
+# TRUTHY sv FALSY value
+# TRUTHY: True
+# FALSY: False
+
+test_falsy = ""
+print("test falsy:", bool(test_falsy))
+
+test_truthy = "MIT"
+print("test truthy:", bool(test_truthy))
