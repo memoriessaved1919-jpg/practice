8db086f54f15deaad8329f836e176da59bc9c4aa
Muhammadumar <memoriessaved1919@gmail.com>
2026-09-13 12:14:50 +0900

    feat:learn string in python

--- primitive.py
+++ primitive.py
@@ -1,4 +1,4 @@
-print("============")
+print("====== number ======")
 count = 100
 count_type = type(count)
 print(f"count: {count} count type: {count_type}")
@@ -6,3 +6,18 @@ print(f"count: {count} count type: {count_type}")
 result1 = count.bit_count()  # method
 result2 = count.numerator  # state
 print(result1, result2)
+
+
+print("====== string ======")
+
+# METHODS: upper() lower() title() find() replace()
+
+course = "AI Python FullStack"
+result = type(course)
+print(f"The type of course: {result}")
+result = course.title()
+print(f"The title: {result}")
+result = course.upper()
+print(f"title upper: {result}")
+result = course.replace("Python FullStack", "Engineering")
+print(f"title replace: {result}")
