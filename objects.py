9fc964d9adb8d1f4245ca8222f90e97ed60473a3
Muhammadumar <memoriessaved1919@gmail.com>
2026-09-15 20:02:21 +0900

    feat: learn python object

---
+++ objects.py
@@ -0,0 +1,28 @@
+'''OBJECTS
+(1) What is object
+(2) Iterable objects & RANGE
+(3) DICTIONARY
+(4) Error handling system
+'''
+
+
+import array  # package/module
+import math
+print("=========")
+
+
+# An object has state and method properties
+# Everything is object in Pythonss
+
+
+print(type("Hello world"))
+print(type(100))
+print(type(True))
+print(type(array))
+print(type(math))
+
+# Paradigms > Functional Programming & OOP
+# OOP 4 CONCEPTS > Abstraction | Encapsulation | Inheitance | Polimorphism
+
+result1 = math.ceil(98.7)  # CALL
+print("resulrt1:", result1)
